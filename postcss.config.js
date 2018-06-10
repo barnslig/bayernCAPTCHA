2698570c3d821db1968b909fdce37a9ef0d5b32c
Leonard Techel <git@barnslig.eu>
2018-06-10 19:00:37 +0200

    css minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 const sass = require('@csstools/postcss-sass');
 
+const plugins = [
+  sass(),
+  autoprefixer(),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(cssnano());
+}
+
 module.exports = {
-  plugins: [
-    sass(),
-    autoprefixer(),
-  ],
+  plugins,
 };
